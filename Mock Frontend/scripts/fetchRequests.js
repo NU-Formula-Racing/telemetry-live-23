e78ce4b9021d15a09f86d8bca90c672b83c5fdaa
ikim-2001 <isaackim2024@u.northwestern.edu>
2022-09-08 15:06:24 -0500

    get metadata and getSession new endpoints

--- Mock Frontend/scripts/fetchRequests.js
+++ Mock Frontend/scripts/fetchRequests.js
@@ -1,11 +1,11 @@
 function getSensorsBySession(sessionName){
     console.log(sessionName);
-        fetch("https://yz8fe8q74j.execute-api.us-east-2.amazonaws.com/default/getSession", 
+        fetch("https://cryptic-headland-94862.herokuapp.com/https://xrqk31hi4e.execute-api.us-east-2.amazonaws.com/default/getSession", 
         {
           method: "POST", 
           headers: { 'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': "*",
-                    "Access-Control-Allow-Methods": "*" },
+          'Access-Control-Allow-Origin': "*"
+        },
           body: JSON.stringify({'nameOfSession': sessionName})
         })
   
@@ -13,3 +13,16 @@ function getSensorsBySession(sessionName){
         .then((response) => document.write(json.parse(response)))
         
       }
+
+function getMetadata(){
+      fetch("https://www.google.com/url?q=https://en2mwj3aze.execute-api.us-east-2.amazonaws.com/getMetadata&sa=D&source=docs&ust=1661181160731536&usg=AOvVaw39DbLEa5I_H_NGeSdAr2h1",
+      {
+        method: "POST", 
+        headers: { 'Content-Type': 'application/json',
+                  'Access-Control-Allow-Origin': "*",
+                  "Access-Control-Allow-Methods": "*" }
+      })
+      .then((res) => res.json())
+      .then((response) => document.write(json.parse(response)))
+      
+    }
